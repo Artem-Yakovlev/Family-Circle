ebd91b5b7b43c31a8f14760be63e8f36fe6bde89
Artem Yakovlev <13.zrka@gmail.com>
2020-01-08 20:01:57 +0300

    beautiful logo

--- app/src/main/java/com/tydeya/familycircle/RegistrationActivity.java
+++ app/src/main/java/com/tydeya/familycircle/RegistrationActivity.java
@@ -12,6 +12,5 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        
     }
 }
