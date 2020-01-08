095517a5864a04a2486f7977869e13bcfc299ef3
Artem Yakovlev <13.zrka@gmail.com>
2020-01-08 18:47:31 +0300

    activity forlogin page

---
+++ app/src/main/java/com/tydeya/familycircle/RegistrationActivity.java
@@ -0,0 +1,17 @@
+package com.tydeya.familycircle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        
+    }
+}
