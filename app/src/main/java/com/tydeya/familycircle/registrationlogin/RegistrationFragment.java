c8a071d1816436ac4b25d6451fe2ed652974dba4
Artem Yakovlev <13.zrka@gmail.com>
2020-01-09 15:31:00 +0300

    Header of registration page xml

--- app/src/main/java/com/tydeya/familycircle/registrationlogin/RegistrationFragment.java
+++ app/src/main/java/com/tydeya/familycircle/registrationlogin/RegistrationFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -12,11 +13,18 @@ import com.tydeya.familycircle.R;
 
 public class RegistrationFragment extends Fragment {
 
+    private View rootView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false);
+
+        rootView = inflater.inflate(R.layout.fragment_registration, container, false);
+
+        // getArguments().getString("EmailFromLogin", "")
+
+        return rootView;
+
     }
 
 
