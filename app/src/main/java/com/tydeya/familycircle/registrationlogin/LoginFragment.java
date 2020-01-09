e00bf6d3a3a295db9215b27aac5811687d5a8244
Artem Yakovlev <13.zrka@gmail.com>
2020-01-09 11:04:40 +0300

    Now the registration activity is using fragments

---
+++ app/src/main/java/com/tydeya/familycircle/registrationlogin/LoginFragment.java
@@ -0,0 +1,88 @@
+package com.tydeya.familycircle.registrationlogin;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.tydeya.familycircle.R;
+
+
+public class LoginFragment extends Fragment implements View.OnClickListener {
+
+    private View rootView;
+
+    private FirebaseAuth auth;
+    private TextInputEditText emailInput;
+    private TextInputEditText passwordInput;
+    private Button signInButton;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Check if user is signed in (non-null) and update UI accordingly.
+        //FirebaseUser currentUser = auth.getCurrentUser();
+        //Toast.makeText(this, "Уже вошел", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        rootView = inflater.inflate(R.layout.fragment_login, container, false);
+
+        auth = FirebaseAuth.getInstance();
+
+        emailInput = rootView.findViewById(R.id.login_page_email_input);
+        passwordInput = rootView.findViewById(R.id.login_page_password_input);
+        signInButton = rootView.findViewById(R.id.login_page_sign_in_button);
+        signInButton.setOnClickListener(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onClick(final View view) {
+        if (view.equals(signInButton)){
+
+
+
+            if (!DataConfirming.isEmptyCheck(emailInput) & !DataConfirming.isEmptyCheck(passwordInput)){
+
+                String userEmail = String.valueOf(emailInput.getText());
+                String userPass = String.valueOf(passwordInput.getText());
+
+                auth.signInWithEmailAndPassword(userEmail, userPass)
+                        .addOnCompleteListener((Activity) view.getContext(), new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(view.getContext(),
+                                            getResources().getText(R.string.login_page_sign_in_button)
+                                                    + " success", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(view.getContext(),
+                                            getResources().getText(R.string.login_page_sign_in_button)
+                                                    + " unsuccessfully", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/tydeya/familycircle/registrationlogin/RegistrationActivity.java
+++ app/src/main/java/com/tydeya/familycircle/registrationlogin/RegistrationActivity.java
@@ -2,11 +2,15 @@ package com.tydeya.familycircle.registrationlogin;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,62 +21,21 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.tydeya.familycircle.R;
 
-public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
+public class RegistrationActivity extends AppCompatActivity {
 
-    private FirebaseAuth auth;
-    private TextInputEditText emailInput;
-    private TextInputEditText passwordInput;
-    private Button signInButton;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        auth = FirebaseAuth.getInstance();
 
-        emailInput = findViewById(R.id.login_page_email_input);
-        passwordInput = findViewById(R.id.login_page_password_input);
-        signInButton = findViewById(R.id.login_page_sign_in_button);
-        signInButton.setOnClickListener(this);
+        Fragment loginFragment = new LoginFragment();
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.registration_activity_main_fragment, loginFragment);
+        fragmentTransaction.commit();
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = auth.getCurrentUser();
-        //Toast.makeText(this, "Уже вошел", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onClick(final View view) {
-        if (view.equals(signInButton)){
-
-
-
-            if (!DataConfirming.isEmptyCheck(emailInput) & !DataConfirming.isEmptyCheck(passwordInput)){
-
-                String userEmail = String.valueOf(emailInput.getText());
-                String userPass = String.valueOf(passwordInput.getText());
-
-                auth.signInWithEmailAndPassword(userEmail, userPass)
-                        .addOnCompleteListener((Activity) view.getContext(), new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(view.getContext(),
-                                            getResources().getText(R.string.login_page_sign_in_button)
-                                                    + " success", Toast.LENGTH_LONG).show();
-                                } else {
-                                    Toast.makeText(view.getContext(),
-                                            getResources().getText(R.string.login_page_sign_in_button)
-                                                    + " unsuccessfully", Toast.LENGTH_LONG).show();
-                                }
-                            }
-                        });
-
-            }
-        }
-    }
 }
