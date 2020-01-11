3b242c2a306f48495a6a28161bec40bb148f0956
Artem Yakovlev <13.zrka@gmail.com>
2020-01-11 19:26:26 +0300

    + valid check for phone number

--- app/src/main/java/com/tydeya/familycircle/firststart/StartInputNumberFragment.java
+++ app/src/main/java/com/tydeya/familycircle/firststart/StartInputNumberFragment.java
@@ -1,7 +1,6 @@
 package com.tydeya.familycircle.firststart;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,27 +10,55 @@ import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.hbb20.CountryCodePicker;
 import com.tydeya.familycircle.R;
 
 
 public class StartInputNumberFragment extends Fragment {
 
-    private MaterialButton nextButton;
     private View root;
     private NavController navController;
+    private MaterialButton nextButton;
+
+    private CountryCodePicker countryPicker;
+    private TextInputEditText phoneNumberInput;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
         root = inflater.inflate(R.layout.fragment_start_input_number, container, false);
 
+        // Processing of country code picker
+        countryPicker = root.findViewById(R.id.start_input_phone_number_ccp);
+        phoneNumberInput = root.findViewById(R.id.start_input_phone_number_input);
+        countryPicker.registerCarrierNumberEditText(phoneNumberInput);
+
+        // Processing of next button
         navController = NavHostFragment.findNavController(this);
-
         nextButton = root.findViewById(R.id.start_input_phone_number_next);
+        nextButton.setOnClickListener(view -> {
+            if (countryPicker.isValidFullNumber()){
+                sendVerificationCode();
+                navController.navigate(R.id.getCodeFromSmsFragment);
+            } else {
+                phoneNumberInput.setError(root.getContext().
+                        getResources().
+                        getString(R.string.start_input_number_invalid_error));
+            }
+        });
 
-        nextButton.setOnClickListener(view -> navController.navigate(R.id.getCodeFromSmsFragment));
         return root;
     }
+
+    /*
+     * This method sends verification code through firebase
+     * */
+    private void sendVerificationCode(){
+
+
+    }
+
 }
+
