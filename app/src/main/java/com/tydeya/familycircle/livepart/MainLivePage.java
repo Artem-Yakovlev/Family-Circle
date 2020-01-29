cb91420270148336370e9c31287b6591349d1620
Artem Yakovlev <13.zrka@gmail.com>
2020-01-29 20:45:41 +0300

    recyclerView with GridLayoutManager

--- app/src/main/java/com/tydeya/familycircle/livepart/MainLivePage.java
+++ app/src/main/java/com/tydeya/familycircle/livepart/MainLivePage.java
@@ -6,15 +6,29 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.tydeya.familycircle.R;
+import com.tydeya.familycircle.family.member.FamilyMember;
+
+import java.util.ArrayList;
 
 public class MainLivePage extends Fragment {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter recyclerViewAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_live_page, container, false);
+        View root = inflater.inflate(R.layout.fragment_main_live_page, container, false);
+
+        ArrayList<FamilyMember> familyMembers = new ArrayList<>();
+        recyclerView = root.findViewById(R.id.main_live_page_family_recycler_view);
+        recyclerViewAdapter = new FamilyMembersRecyclerView(getContext(), familyMembers);
+        recyclerView.setAdapter(recyclerViewAdapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        return root;
     }
 }
