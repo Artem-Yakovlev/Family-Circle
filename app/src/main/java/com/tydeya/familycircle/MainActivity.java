71062c2e1187b25df41faa97abb59ed1dfc2fe1a
Artem Yakovlev <13.zrka@gmail.com>
2020-02-05 18:08:03 +0300

    prepare for main navigation

--- app/src/main/java/com/tydeya/familycircle/MainActivity.java
+++ app/src/main/java/com/tydeya/familycircle/MainActivity.java
@@ -2,10 +2,14 @@ package com.tydeya.familycircle;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.tydeya.familycircle.firststart.FirstStartActivity;
 
@@ -15,20 +19,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.main_bottom_navigation_view);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.mainLivePage,
+                R.id.mainPlanPage, R.id.mainConversationPage, R.id.mainMapPage, R.id.mainPanelPage)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.main_fragment_container);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         FirebaseAuth auth = FirebaseAuth.getInstance();
-        
+
         if (auth.getCurrentUser() == null) {
             Intent intent = new Intent(this, FirstStartActivity.class);
             startActivity(intent);
             this.finish();
-        } else {
-            Toast.makeText(this, getString(R.string.main_activity_registred_user),
-                    Toast.LENGTH_LONG).show();
         }
     }
 }
