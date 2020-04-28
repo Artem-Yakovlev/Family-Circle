c8a96edb1a7406d2e790ba8c1156f45cdf640a30
Artem-Yakovlev <13.zrka@gmail.com>
2020-04-28 22:20:58 +0300

    refactor: lifecycle-aware structure of dialog fragment

--- app/src/main/java/com/tydeya/familycircle/ui/planpart/kitchenorganizer/pages/foodforbuy/alllists/CreateBuyListDialog.kt
+++ app/src/main/java/com/tydeya/familycircle/ui/planpart/kitchenorganizer/pages/foodforbuy/alllists/CreateBuyListDialog.kt
@@ -3,90 +3,72 @@ package com.tydeya.familycircle.ui.planpart.kitchenorganizer.pages.foodforbuy.al
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
-import com.tydeya.familycircle.App
 import com.tydeya.familycircle.R
 import com.tydeya.familycircle.databinding.DialogFoodForBuyNewListBinding
-import com.tydeya.familycircle.databinding.FragmentFoodForBuyBinding
-import com.tydeya.familycircle.domain.kitchenorganizer.kitchenorhanizerinteractor.details.KitchenOrganizerInteractor
 import com.tydeya.familycircle.viewmodel.AllBuyCatalogsViewModel
-import kotlinx.android.synthetic.main.dialog_food_for_buy_new_list.view.*
-import javax.inject.Inject
 
 
 class CreateBuyListDialog : DialogFragment() {
 
+    private lateinit var allBuyCatalogsViewModel: AllBuyCatalogsViewModel
+
     private var _binding: DialogFoodForBuyNewListBinding? = null
-    private var allBuyCatalogsViewModel: AllBuyCatalogsViewModel? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        App.getComponent().injectDialog(this)
-        Log.d("ASMR", "onCreate")
-        allBuyCatalogsViewModel = ViewModelProvider(requireParentFragment())
-                .get(AllBuyCatalogsViewModel::class.java)
-    }
+    private lateinit var root: View
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        Log.d("ASMR", "onCreateDialog")
-        val builder = AlertDialog.Builder(activity)
-        val inflater = requireActivity().layoutInflater
-        val view = inflater.inflate(R.layout.dialog_food_for_buy_new_list, null)
+        root = requireActivity().layoutInflater.inflate(R.layout.dialog_food_for_buy_new_list, null)
 
-        //binding = DialogFoodForBuyNewListBinding.inflate(inflater, view, false)
+        return AlertDialog.Builder(activity).apply {
+            setView(root)
+        }.create()
+    }
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = DialogFoodForBuyNewListBinding.bind(root)
+        allBuyCatalogsViewModel = ViewModelProvider(requireParentFragment())
+                .get(AllBuyCatalogsViewModel::class.java)
+        return binding.root
+    }
 
-        builder.setView(view)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        view.dialog_shopping_list_create_button.setOnClickListener {
+        binding.dialogShoppingListCreateButton.setOnClickListener {
 
             val createFlag: Boolean
-            val title = view.dialog_shopping_list_name.text.toString().trim()
+            val title = binding.dialogShoppingListName.text.toString().trim()
 
             if (title == "") {
-                view.dialog_shopping_list_name.error = view.context!!
+                binding.dialogShoppingListName.error = view.context!!
                         .resources.getString(R.string.empty_necessary_field_warning)
                 createFlag = false
             } else {
-                createFlag = allBuyCatalogsViewModel?.ifPossibleThenCreateBuysCatalog(title)
-                        ?: false
+                createFlag = allBuyCatalogsViewModel.ifPossibleThenCreateBuysCatalog(title)
             }
 
             if (createFlag) {
                 dismiss()
             } else {
-                view.dialog_shopping_list_name.error = view.context!!
+                binding.dialogShoppingListName.error = view.context!!
                         .resources.getString(R.string.dialog_new_buy_list_already_exist)
             }
         }
 
-        view.dialog_shopping_list_cancel_button.setOnClickListener {
+        binding.dialogShoppingListCancelButton.setOnClickListener {
             dismiss()
         }
-
-        return builder.create()
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        Log.d("ASMR", "onCreateView")
-        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
-    }
-
-    companion object {
-        fun newInstance() {
-
-        }
     }
 
 }
